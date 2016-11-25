4fded5d9400423777a95b8b64a6524c1a50fd88e
Markus Gritsch <m.gritsch@gmail.com>
2016-11-25 09:03:39 +0100

    Added functionality to switch a GPIO pin using a channel on the TX.

--- H101_dual/src/hardware.h
+++ H101_dual/src/hardware.h
@@ -6,8 +6,9 @@
 
 #define RGB_LED_NUMBER 0
 // pin / port for the RGB led ( programming port ok )
-#define RGB_PIN GPIO_PIN_13
+#define RGB_PIN GPIO_PIN_13 // SWDAT
 #define RGB_PORT GPIOA
 
 
-
+#define FPV_PIN GPIO_PIN_14 // SWCLK
+#define FPV_PIN_PORT GPIOA
